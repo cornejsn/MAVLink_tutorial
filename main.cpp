0ec119a469aaeadc174d80ea361008fea3f4e425
cornejsn <cornejsn@gmail.com>
2021-10-06 02:09:13 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,10 +1,21 @@
 #include <iostream>
 #include "src\pugixml.hpp"
+#include "src\pugixml.cpp"
 
 using namespace std;
 
+template <class XMLParser>
+XMLParser ParseXML (XMLParser xml) {
+    pugi::xml_document doc;
+    pugi::xml_parse_result result = doc.load_file(xml);
 
+    cout << "Load result: " << result.description() << ", mesh name: " << doc.child("mesh").attribute("name").value() << endl;
+    return 0;
+}
 
 int main() {
-    cout << "Hello World" << endl;
+    const char* xml_path = "docs/samples/tree.xml";
+    ParseXML(xml_path);
+
+    return 0;
 }
